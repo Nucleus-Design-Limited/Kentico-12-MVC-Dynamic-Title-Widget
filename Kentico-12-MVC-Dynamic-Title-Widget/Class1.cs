c847d000eb0bbd7ad62f3a440ae64a1e8ba151c6
Danny Winbourne <dwinbourne@nucleus.co.uk>
2019-12-10 12:39:07 +0000

    Initial project commit

---
+++ Kentico-12-MVC-Dynamic-Title-Widget/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentico_12_MVC_Dynamic_Title_Widget
+{
+    public class Class1
+    {
+    }
+}
